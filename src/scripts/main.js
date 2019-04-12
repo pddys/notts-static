f8196660f2f2fcacc9e1b93983d886f3b9e21215
pddys <c3xh5i@gmail.com>
2019-04-12 20:46:59 +0100

    ADD: Messy first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,6 +7,31 @@ import jQuery from 'jquery';
     .removeClass('no-js')
     .addClass('js');
 
+	const navTrigger = $('#c-nav__trigger')
+	const navigationTag = $('.c-nav')
+	const bodyTag = $('body')
+
+	navTrigger.on("click", function(e) {
+		navTrigger.toggleClass('is-active')
+		navigationTag.toggleClass('is-active')
+		bodyTag.toggleClass('menu-open')
+	})
+
+	const secNavTrigger = $('#js-tertiary-nav--trigger')
+	const secNavigationTag = $('.c-tertiary-nav')
+	const secNavigationBG = $('.c-tertiary-nav__bg')
+
+	secNavTrigger.on("click", function(e) {
+		e.preventDefault();
+		secNavTrigger.toggleClass('is-active')
+		$(this).parents('li').toggleClass('is-active')
+		secNavigationBG.toggleClass('is-active')
+		bodyTag.toggleClass('tertiary-menu-open')
+	})
+
+
+
+
   // END JQUERY.
 })(jQuery);
 
